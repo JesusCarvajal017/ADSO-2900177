d0b9c7b263ea70d54dd6f3cb23e5a565416ccbae
Jesus Carvajal <jesusfernandocarvajal2018@gmail.com>
2024-05-26 18:52:15 -0500

    tabla completada

---
+++ tecnico/2024/mayo/20-05-2024/tablaPeriodicaB/js/data_tabla_periodica.js
@@ -0,0 +1,21 @@
+
+const dataTable = [
+    {
+        id: 1,
+        nombreElement: 'Hidrogeno',
+        simbolo: 'H',
+        masaAtomic: 1.00764,
+        numeroAtomic: 1,
+        densidad: 13,
+        electronegatividad: 2.20,
+    },
+    {
+        id: 1,
+        nombreElement: 'Hidrogeno',
+        simbolo: 'H',
+        masaAtomic: 1.00764,
+        numeroAtomic: 1,
+        densidad: 13,
+        electronegatividad: 2.20,
+    },
+]
---
+++ tecnico/2024/mayo/20-05-2024/tablaPeriodicaB/js/main.js
@@ -0,0 +1,43 @@
+/**
+ * Autor: Jesus Fernando Carvajal Anacona
+ */
+
+// const infoTabla = dataTable;
+
+
+// let elmento = `<div class="elemento no-metales">
+//                 <div class="masa-atomic"></div>
+//                 <div class="numero-atomic"></div>
+//                 <div class="densidad"></div>
+//                 <div class="electro"></div>
+//                 <div class="simbol"></div>
+//                 <div class="name-element"></div>
+//             </div>`;
+
+// let nombreElemento = document.querySelector();
+// let simbolo = document.querySelector();
+// let masaAtomic = document.querySelector();
+// let numeroAtomic = document.querySelector();
+// let densidad = document.querySelector();
+// let electronegatividad = document.querySelector();
+
+// console.log(infoTabla)
+let elemento = document.getElementById('elemento1');
+
+let nombreElemento = document.querySelector('.name-element1').innerHTML;
+// let simbolo = document.querySelector('simbol1').value;
+// let masaAtomic = document.querySelector('masa-atomic1').value;
+// let numeroAtomic = document.querySelector('numero-atomic1').value;
+// let densidad = document.querySelector('densidad1').value;
+// let electronegatividad = document.querySelector('electro1').value;
+
+
+// let mostrar = document.querySelector('.jesus');
+// elemento.addEventListener('mouseover', ()=>{
+//     mostrar.innerHTML = nombreElemento;
+    
+//     elemento.addEventListener('mouseout', ()=>{
+//         mostrar.innerHTML = '';
+//     });
+// })
+
